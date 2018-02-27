fb658669ac0595e669c32f6cef39d48863f4cabf
jandie <eidnaj@live.nl>
2018-02-27 13:56:53 +0100

    Vehicle dao improved

--- government/src/main/java/dao/VehicleDao.java
+++ government/src/main/java/dao/VehicleDao.java
@@ -24,7 +24,7 @@ public class VehicleDao {
     public List<Vehicle> GetAllVehiclesInCountry(String countryName) {
         TypedQuery<Vehicle> query
                 = em.createQuery("SELECT v FROM Vehicle v " +
-                "WHERE v.currentLocation.country.name = :countryName", Vehicle.class);
+                "JOIN v.currentLocation l WHERE l.name = :countryName", Vehicle.class);
 
         return query.setParameter("countryName", countryName).getResultList();
     }
