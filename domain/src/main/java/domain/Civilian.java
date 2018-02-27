c877e52c22dc29579a851dcd75cbdb12b05dbcaa
jandie <eidnaj@live.nl>
2018-02-27 13:26:02 +0100

    Civillian class updated

--- domain/src/main/java/domain/Civilian.java
+++ domain/src/main/java/domain/Civilian.java
@@ -1,4 +1,43 @@
 package domain;
 
-public class Civilian extends User {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+public class Civilian extends User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToMany
+    private List<Vehicle> vehicles;
+    @ManyToOne
+    private List<Invoice> invoices;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(List<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+
+    public List<Invoice> getInvoices() {
+        return invoices;
+    }
+
+    public void setInvoices(List<Invoice> invoices) {
+        this.invoices = invoices;
+    }
 }
