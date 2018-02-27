005f7030eefd3cdce194df0a40f95db22070911a
guillaime <guillaimejanssen@msn.com>
2018-02-27 15:04:49 +0100

    Added methods to the service and resource

--- government/src/main/java/rest/InvoiceResource.java
+++ government/src/main/java/rest/InvoiceResource.java
@@ -1,7 +1,26 @@
 package rest;
 
-import javax.ejb.Stateless;
+import domain.Invoice;
+import service.InvoiceService;
 
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import java.util.List;
+
+@Path("invoices")
 @Stateless
 public class InvoiceResource {
+
+    @Inject
+    InvoiceService invoiceService;
+
+    @GET
+    @Produces("application/json")
+    public List<Invoice> getAllInvoices(){
+        return invoiceService.getAllInvoices();
+    }
 }
--- government/src/main/java/service/InvoiceService.java
+++ government/src/main/java/service/InvoiceService.java
@@ -1,7 +1,19 @@
 package service;
 
+import dao.InvoiceDao;
+import domain.Invoice;
+
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+import java.util.List;
 
 @Stateless
 public class InvoiceService {
+
+    @Inject
+    InvoiceDao invoiceDao;
+
+    public List<Invoice> getAllInvoices(){
+        return  invoiceDao.getAllInvoices();
+    }
 }
