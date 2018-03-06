1f72105b85a74fc171658c3a9d681905a10e3ff5
guillaime <guillaimejanssen@msn.com>
2018-03-06 22:57:24 +0100

    Added filtering to invoiceResource Created a new invoice state and added it to invoice

--- src/main/java/domain/Invoice.kt
+++ src/main/java/domain/Invoice.kt
@@ -1,27 +1,29 @@
 package domain
 
 import domain.enums.InvoiceGenerationType
+import domain.enums.InvoiceState
 
 import javax.persistence.*
 import java.io.Serializable
 import java.util.Date
 
 @Entity
+@NamedQuery(name = "Invoice.allInvoices", query = "SELECT i FROM Invoice i")
 class Invoice : Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long? = null
-
     @Column
     var createdOn: Date? = null
     @Column
     var generatedFor: Date? = null
     @Enumerated(EnumType.STRING)
     var generationType: InvoiceGenerationType? = null
+    @Enumerated(EnumType.STRING)
+    var state: InvoiceState? = null
     @Column
     var totalPrice: Double = 0.toDouble()
-
     @ManyToOne
     var civilian: Civilian? = null
     @ManyToOne
--- src/main/java/service/InvoiceService.kt
+++ src/main/java/service/InvoiceService.kt
@@ -2,9 +2,14 @@ package service
 
 import dao.InvoiceDao
 import domain.Invoice
+import domain.enums.InvoiceGenerationType
+import domain.enums.InvoiceState
+import java.sql.Timestamp
+import java.util.*
 
 import javax.ejb.Stateless
 import javax.inject.Inject
+import javax.swing.text.html.parser.Parser
 
 @Stateless
 class InvoiceService @Inject constructor(
@@ -13,5 +18,17 @@ class InvoiceService @Inject constructor(
 
     fun allInvoices(): List<Invoice> = invoiceDao.allInvoices();
 
-    fun getInvoiceById(id: Long?): Invoice? = invoiceDao.getInvoiceById(id)
+    fun getInvoiceById(id: String): Invoice = invoiceDao.getInvoiceById(id)
+
+    fun allInvoicesByVehicle(id: String): List<Invoice> = invoiceDao.allInvoicesByVehicle(id);
+
+    fun allInvoicesByCivilian(id: String): List<Invoice> = invoiceDao.allInvoicesByCivilian(id);
+
+    fun allInvoicesCreatedBetweenDates(start: String, end: String): List<Invoice> = invoiceDao.allInvoicesCreatedBetweenDates(Date(start.toLong()), Date(end.toLong()))
+
+    fun allInvoicesForBetweenDates(start: String, end: String): List<Invoice> = invoiceDao.allInvoicesForBetweenDates(Date(start.toLong()), Date(end.toLong()))
+
+    fun allInvoicesGeneratedBy(type: InvoiceGenerationType): List<Invoice> = invoiceDao.allInvoicesGeneratedBy(type)
+
+    fun allInvoicesByState(state: InvoiceState): List<Invoice> = invoiceDao.allInvoicesByStatus(state)
 }
