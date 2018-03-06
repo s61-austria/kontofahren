6f560d1b07fdc4865314bcca570f94cac2cc9ff6
Yannic <zebybez@gmail.com>
2018-03-06 13:56:03 +0100

    removed wrong imports causing issues, moved correct files from old folder to new one.

--- src/main/java/dao/UserDao.kt
+++ src/main/java/dao/UserDao.kt
@@ -1,3 +1,33 @@
 package dao
 
-class UserDao
+import domain.User
+
+import javax.ejb.Stateless
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+import javax.persistence.TypedQuery
+
+@Stateless
+class UserDao {
+    @PersistenceContext
+    internal var em: EntityManager? = null
+
+    val allUsers: List<User>
+        get() {
+            val query = em!!.createQuery("SELECT u FROM KontoUser u", User::class.java)
+
+            return query.resultList
+        }
+
+    fun persistUser(user: User): User {
+        em!!.persist(user)
+
+        return user
+    }
+
+    fun getUserById(id: Long): User {
+        val query = em!!.createQuery("SELECT u FROM User u WHERE u.id = :id", User::class.java)
+
+        return query.singleResult
+    }
+}
