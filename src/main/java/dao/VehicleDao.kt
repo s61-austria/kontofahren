dedf587ae23fe8821043600098b73e612e0168ce
Yannic <zebybez@gmail.com>
2018-03-06 12:09:54 +0100

    commit before stash apply

--- src/main/java/dao/VehicleDao.kt
+++ src/main/java/dao/VehicleDao.kt
@@ -15,18 +15,23 @@ class VehicleDao {
 
     val allVehicles: List<Vehicle>
         get() {
-            val query = em!!.createQuery("SELECT v FROM Vehicle v", Vehicle::class.java)
+            val query = em!!.createQuery<Vehicle>("SELECT v FROM Vehicle v", Vehicle::class.java)
 
             return query.resultList
         }
 
     fun getAllVehiclesInCountry(countryName: String): List<Vehicle> {
-        val query = em!!.createQuery("SELECT v FROM Vehicle v " + "JOIN v.currentLocation l JOIN l.country c WHERE c.name = :countryName", Vehicle::class.java)
+        val query = em!!.createQuery<Vehicle>("SELECT v FROM Vehicle v " + "JOIN v.currentLocation l JOIN l.country c WHERE c.name = :countryName", Vehicle::class.java)
 
         return query.setParameter("countryName", countryName).resultList
     }
 
-    fun addVehicle(vehicle: Vehicle): Vehicle {
+    fun getVehicleById(id: Long): Vehicle {
+        val query = em!!.createQuery<Vehicle>("SELECT v FROM Vehicle v WHERE v.id = :id", Vehicle::class.java)
+        return query.singleResult
+    }
+
+    fun persistVehicle(vehicle: Vehicle): Vehicle {
         em!!.persist(vehicle)
 
         return vehicle
