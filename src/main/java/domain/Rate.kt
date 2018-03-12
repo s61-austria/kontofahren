7b2e5ffe0b0d4e0522bc22befcff3fdfd19649a3
Michel Mans <michelmans@users.noreply.github.com>
2018-03-12 15:47:56 +0100

    Removed uncommented code

--- src/main/java/domain/Rate.kt
+++ src/main/java/domain/Rate.kt
@@ -15,12 +15,6 @@ class Rate : Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long? = null
 
-    /*@Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid2")
-    @Column(unique = true)
-    val uuid: UUID? = null*/
-
     @Enumerated(EnumType.STRING)
     var vehicleType: VehicleType? = null
     @Column
@@ -31,7 +25,6 @@ class Rate : Serializable {
     constructor() {}
 
     constructor(vehicleType: VehicleType, kmPrice: Double, vignetteType: VignetteType) {
-        //this.id = UUID.randomUUID()
         this.vehicleType = vehicleType
         this.kmPrice = kmPrice
         this.vignetteType = vignetteType
