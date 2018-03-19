fed80c2cd2ea9ac10b331cc65dead8f3af793d72
jandie <eidnaj@live.nl>
2018-03-19 09:30:26 +0100

    Added necessary dependency to gradle to be able to use api

--- src/main/java/rest/VehicleResource.kt
+++ src/main/java/rest/VehicleResource.kt
@@ -1,10 +1,15 @@
 package rest
 
-import domain.Vehicle
 import domain.enums.VehicleType
 import service.VehicleService
 import javax.inject.Inject
-import javax.ws.rs.*
+import javax.ws.rs.Consumes
+import javax.ws.rs.GET
+import javax.ws.rs.POST
+import javax.ws.rs.PUT
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
 import javax.ws.rs.core.Response
 
 @Path("vehicles")
