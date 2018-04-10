98484861f33e30d911025b6fd2a24874c2dbef31
requinard <d.diks94@gmail.com>
2018-04-10 14:11:10 +0200

    feat(mq): add mq connector

---
+++ src/integrationTest/kotlin/messaging/RabbitGatewayIntegration.kt
@@ -0,0 +1,25 @@
+package messaging
+
+import messaging.Exchange.LOCATION_EXCHANGE
+import messaging.Queue.FRONTEND_LOCATION_UPDATE
+import messaging.Routing.EMPTY
+import org.junit.Test
+
+class RabbitGatewayIntegration {
+    val rabbitGateway by lazy { RabbitGateway() }
+
+    @Test
+    fun basicPublish(){
+        val obj = mapOf("hello" to "world")
+        rabbitGateway.publish(LOCATION_EXCHANGE,obj, EMPTY)
+    }
+
+    @Test
+    fun basicConsume(){
+        rabbitGateway.consume(FRONTEND_LOCATION_UPDATE, {
+            println(it)
+            return@consume
+        })
+        Thread.sleep(2000)
+    }
+}
