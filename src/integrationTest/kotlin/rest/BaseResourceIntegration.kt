efff667fc35cc41187d8a7960a74fa3d1a7c1abf
requinard <d.diks94@gmail.com>
2018-04-17 10:34:01 +0200

    Add function to get user from http headers

--- src/integrationTest/kotlin/rest/BaseResourceIntegration.kt
+++ src/integrationTest/kotlin/rest/BaseResourceIntegration.kt
@@ -4,6 +4,8 @@ import io.restassured.RestAssured
 import org.junit.Before
 
 abstract class BaseResourceIntegration {
+    val token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJrb250b2ZhaHJlbi13aWRsZmx5IiwiZ3JvdXBzIjpbXSwiaWQiOiJhMzgzNjJjMi1hNWFmLTRiYTYtODIyMi02NjZlZGNiZWI5ZjMiLCJleHAiOjE1MjY1NDQ4MDQsImlhdCI6MTUyMzk1MjgwNCwidXNlcm5hbWUiOiJNaWNoZWwgTWFucyJ9.AQ3VLwCqHzR2Zr3RIpdEdvfmZN-BP699wAw1vFsmxQk"
+
     @Before
     fun setupRestAssured() {
         RestAssured.baseURI = "http://localhost"
